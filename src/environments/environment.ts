c90ff6df5bafef9905d7997d8bc68cf232323258
KhairatMouhcine <khairatmouhcine125@gmail.com>
2025-07-21 09:15:00 +0100

    chore: setup environment variables
    Co-authored-by: Youssef <youssefmae26082000@gmail.com>
    Co-authored-by: bassma <b.chihab2002@gmail.com>

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiBase: 'https://localhost:7112/api/factures',
+  profilApiBaseUrl: 'http://localhost:5285', // <-- port de ProfilMS
+  documentsApiBaseUrl: 'https://localhost:7132/api', // port HTTPS de DocumentsMS
+  vendorsApiBaseUrl:   'https://localhost:7285/api'  
+};
