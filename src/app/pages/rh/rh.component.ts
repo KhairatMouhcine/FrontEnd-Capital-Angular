d105192c70604c05e7a2549cc08f9be15d812ce7
KhairatMouhcine <khairatmouhcine125@gmail.com>
2025-08-18 09:30:00 +0100

    fix(matching): corrige erreur matching
    Co-authored-by: Youssef <youssefmae26082000@gmail.com>
    Co-authored-by: bassma <b.chihab2002@gmail.com>

---
+++ src/app/pages/rh/rh.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from "../dashboard/dashboard.component";
+
+@Component({
+  selector: 'app-rh',
+  imports: [DashboardComponent],
+  templateUrl: './rh.component.html',
+  styleUrl: './rh.component.css'
+})
+export class RhComponent {
+
+}
