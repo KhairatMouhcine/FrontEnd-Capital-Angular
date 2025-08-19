81b7ef4aeec9d28d16af1f9656c3610c7ee8a3bf
KhairatMouhcine <khairatmouhcine125@gmail.com>
2025-08-19 11:00:00 +0100

    feat(auth): LOGIN PRESTATAIRE
    Co-authored-by: Youssef <youssefmae26082000@gmail.com>
    Co-authored-by: bassma <b.chihab2002@gmail.com>

---
+++ src/app/pages/prestataire/prestataire.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DashboardComponent } from "../dashboard/dashboard.component";
+
+@Component({
+  selector: 'app-prestataire',
+  imports: [DashboardComponent],
+  templateUrl: './prestataire.component.html',
+  styleUrl: './prestataire.component.css'
+})
+export class PrestataireComponent {
+
+}
